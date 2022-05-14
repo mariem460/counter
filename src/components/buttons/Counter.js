f177f142d7d132d73bcc0b44cb1162446fa00511
mariem460 <mariem.oueslati.hajjem@gmail.com>
2022-05-14 14:37:28 +0200

    add controls

--- src/components/buttons/Counter.js
+++ src/components/buttons/Counter.js
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import './Counter.css';
+import PropTypes from "prop-types";
+
 function Counter() {
     const [count, setCount] = useState(0)
     const decrement = () => {
@@ -17,5 +19,11 @@ function Counter() {
         <h3>Counter</h3>
       </div>
     );
+  }
+  Counter.prototype = {
+    label: PropTypes.string,
+    backgroundColor: PropTypes.string,
+    size: PropTypes.oneOf(["sm", "md", "lg"]),
+    onClick: PropTypes.func
   }
   export default Counter;
